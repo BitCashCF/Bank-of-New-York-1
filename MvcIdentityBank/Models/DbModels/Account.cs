005d99e56e6d57a38c76409f3d79f421ed2485e6
FailyFox <FailyFox@A33COMP15>
2019-02-26 20:32:42 +0200

    Qwerty

---
+++ MvcIdentityBank/Models/DbModels/Account.cs
@@ -0,0 +1,12 @@
+﻿namespace MvcIdentityBank
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public decimal Qty { get; set; }
+
+        //navigation property
+        public virtual CustomUser CustomUser { get; set; }
+    }
+}
