0d987687aa41e7fc9659a08fb6cb2fcc97f4a678
FailyFox <FailyFox@A33COMP15>
2019-02-28 21:08:11 +0200

    Changes added

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index(string id)
+        {
+            return View("Index", (object)id);
+        }
+
+        public ActionResult OrderedProducts(string id)
+        {
+            var orders = OrdersDb.GetOrders;
+            if (!String.IsNullOrEmpty(id) && id != "All")
+            {
+                orders = orders.Where(o => o.Customer == id);
+            }
+
+            return PartialView(orders);
+        }
+    }
+}
